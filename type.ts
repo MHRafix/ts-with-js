4aa943b65862994b425f4669575811f0c33009fc
MHRafix <fmhrafiz27@gmail.com>
2022-09-15 16:12:05 +0600

    learning ts

---
+++ type.ts
@@ -0,0 +1,21 @@
+// normal veriable type
+let a: string;
+
+// union type
+let b: string | number | boolean;
+
+// union type array
+let c: (number | string | boolean | object)[];
+
+// any type
+let d: any;
+
+// object type
+let obj: {
+	name: string;
+	age: number;
+	isAdult: boolean;
+};
+
+// array type
+let array = [];
