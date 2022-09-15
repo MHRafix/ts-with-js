cf1847d87ac2d0cc83a07462212823ba6771d8a7
MHRafix <fmhrafiz27@gmail.com>
2022-09-15 16:22:54 +0600

    ts data type learning done:-

--- type.ts
+++ type.ts
@@ -1,21 +1,36 @@
-// normal veriable type
+// normal type veriable
 let a: string;
 
 // union type
 let b: string | number | boolean;
 
+// normal type array
+let array: string[] = [];
+array.push('Tweleve 12');
+
 // union type array
-let c: (number | string | boolean | object)[];
+let c: (number | string | boolean | object)[] = [];
 
-// any type
-let d: any;
+c.push(12, 'fhdjhfjdf', true);
 
-// object type
-let obj: {
+// normal type object
+let obj: object;
+
+obj = {
+	name: 'Mehedi',
+	age: '20',
+	isAdult: 'true',
+};
+
+// union || schema type object
+let obj2: {
 	name: string;
 	age: number;
 	isAdult: boolean;
 };
 
-// array type
-let array = [];
+obj2 = {
+	name: 'Mehedi',
+	age: 20,
+	isAdult: true,
+};
